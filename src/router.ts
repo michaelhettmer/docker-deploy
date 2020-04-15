e68cd6c1c973876b5170f4596479acfc48467208
Michael Hettmer <mail@michael-hettmer.de>
2020-04-15 11:01:25 +0200

    fix: improve configuration file loading in router

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,17 @@
 import fs from 'fs';
+import path from 'path';
 import yaml from 'yaml';
 import { Router } from 'express';
 
-const deploymentFile = fs.readFileSync('./deployment.yml', 'utf8');
-const deployment = yaml.parse(deploymentFile);
-console.log(deployment);
+const configPath = path.resolve('./deployment.yml');
+const configFile = fs.readFileSync(configPath, 'utf8');
+const config = yaml.parse(configFile);
+
+console.log('configuration loaded successfully', config);
 
 const router = Router();
 router.get('/test', (_req, res) => {
-    res.send(200);
+    res.sendStatus(200);
 });
 
 export default router;
