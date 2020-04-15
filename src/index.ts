7a2475f24209e09d55fcec207e93192180f7f4a0
Michael Hettmer <mail@michael-hettmer.de>
2020-04-15 09:47:23 +0200

    feat: add basic express webserver

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { Server as HttpServer } from 'http';
+import bodyParser from 'body-parser';
+import cors, { CorsOptions } from 'cors';
+import { serverPort } from './config';
+
+const whitelist = ['http://localhost:3000', 'http://localhost:8000'];
+
+export const CorsWhitelist: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(CorsWhitelist));
+
+const server = new HttpServer(app);
+server.listen(serverPort, () => {
+    console.log('### Server listening on *:' + serverPort);
+});
